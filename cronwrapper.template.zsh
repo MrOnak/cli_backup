aaa3a4dbf3f987be68fbdca8d2c6703395c3c4b7
Dominique Stender <oldaric.gw2@gmail.com>
2022-11-18 22:56:50 +0100

    added template for a crontab wrapper script

---
+++ cronwrapper.template.zsh
@@ -0,0 +1,40 @@
+#!/usr/bin/env zsh
+
+# This is an example script you can use to wrap a backup and/or cloudify call
+# for use within a cron job.
+#
+# It'll take care of redirecting STDOUT and STDERR to a temporary logfile.
+# It'll send the logfile via email after the job is done
+# It'll append the logfile of the run to a permanent logfile and delete the temporary one.
+# 
+# does need some editing to make it suit your needs, NOT EVERYTHING IS A VARIABLE
+# TAKE CARE TO REPLACE ALL /path/to's IN THE backup.zsh CALL ITSELF
+
+LOGDIR=/var/log
+TMPLOG=$LOGDIR/clibackup-`date +%Y%m%d-%H%M%S`.log
+LOG=$LOGDIR/clibackup.log
+MAIL_SUBJECT="backup of $HOST completed"
+MAIL_RECEIVER="$USER"
+
+# create log header
+echo > $TMPLOG
+echo "===========================================================" >> $TMPLOG
+echo "starting new backup: "`date` >> $TMPLOG
+echo >> $TMPLOG
+
+# do create the backup
+/path/to/backup.zsh \
+	-y \
+	-s /path/to/sources \
+	-d file:///path/to/destination/ \
+	-x /path/to/backup.excludes.txt \
+	-e /path/to/env.parameters.sh \
+	>> $TMPLOG 2>&1
+
+# send logfile via mail
+cat $TMPLOG | mail -s $MAIL_SUBJECT $MAIL_RECEIVER
+
+# append logfile to overall log and delete temporary one
+cat $TMPLOG >> $LOG
+rm $TMPLOG
+
