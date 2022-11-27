ba0a568ebf547c3fe442eb5ae8d20510a884a69e
Dominique Stender <oldaric.gw2@gmail.com>
2022-11-27 16:36:48 +0100

    improved cronwrapper with runtime information

--- automation/cronwrapper.zsh
+++ automation/cronwrapper.zsh
@@ -10,16 +10,18 @@
 # does need some editing to make it suit your needs, NOT EVERYTHING IS A VARIABLE
 # TAKE CARE TO REPLACE ALL /path/to's IN THE backup.zsh CALL ITSELF
 
+TIME_START=$(date +%s)
+
 LOGDIR=/var/log
-TMPLOG=$LOGDIR/clibackup-`date +%Y%m%d-%H%M%S`.log
+TMPLOG=$LOGDIR/clibackup-$(date -d @${TIME_START} +%Y%m%d-%H%M%S).log
 LOG=$LOGDIR/clibackup.log
 MAIL_SUBJECT="backup of $HOST completed"
 MAIL_RECEIVER="$USER"
 
 # create log header
 echo > $TMPLOG
-echo "===========================================================" >> $TMPLOG
-echo "starting new backup: "`date` >> $TMPLOG
+echo "====================" >> $TMPLOG
+echo "starting new backup: "$(date -d @${TIME_START}) >> $TMPLOG
 echo >> $TMPLOG
 
 # do create the backup
@@ -30,6 +32,15 @@ echo >> $TMPLOG
 	-x /path/to/backup.excludes.txt \
 	-e /path/to/env.parameters.sh \
 	>> $TMPLOG 2>&1
+
+# create log footer
+TIME_END=$(date +%s)
+TIME_ELAPSED=$(( $TIME_END - $TIME_START ))
+
+echo >> $TMPLOG
+echo "backup complete after: $(date -u -d @${TIME_ELAPSED} +%T) "
+echo "====================" >> $TMPLOG
+echo >> $TMPLOG
 
 # send logfile via mail
 cat $TMPLOG | mail -s $MAIL_SUBJECT $MAIL_RECEIVER
